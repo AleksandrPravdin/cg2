4382b0b44cb1c51eaf61450ffa229fcfbb45e51e
Alexander Gubanov <sanygubanov4@mail.ru>
2023-11-06 22:15:22 +0300

    update drawSector

--- src/main/java/com/example/task02_07/Rasterization.java
+++ src/main/java/com/example/task02_07/Rasterization.java
@@ -13,31 +13,31 @@ public class Rasterization {
             final double angle1, final double angle2,
             final Color color) {
         final PixelWriter pixelWriter = graphicsContext.getPixelWriter();
-        final double inclinationСoefficient1 = Math.tan(Math.toRadians(angle1));
-        final double inclinationСoefficient2 = Math.tan(Math.toRadians(angle2));
-        final int quarterangle1 = defineQuarterCircle(angle1);
-        final int quarterangle2 = defineQuarterCircle(angle2);
+        final double inclinationCoefficient1 = Math.tan(Math.toRadians(angle1));
+        final double inclinationCoefficient2 = Math.tan(Math.toRadians(angle2));
+        final int quarterAngle1 = defineQuarterCircle(angle1);
+        final int quarterAngle2 = defineQuarterCircle(angle2);
 
         for (int row = y - radius; row < y + radius; row++) {
             int xLeft = (int) (x - Math.round(Math.sqrt(radius * radius - (row - y) * (row - y))));
             int xRight = (int) (x + Math.round(Math.sqrt(radius * radius - (row - y) * (row - y))));
             for (int col = xLeft; col < xRight; col++) {
-                switch (quarterangle1) {
+                switch (quarterAngle1) {
                     case 1:
-                        switch (quarterangle2) {
+                        switch (quarterAngle2) {
                             case 1:
                             case 2:
-                                if ((angle2>=angle1)&&(col >= ((y - row) / inclinationСoefficient2) + x) && (col <= ((y - row) / inclinationСoefficient1) + x)) {
+                                if ((angle2 >= angle1) && (col >= ((y - row) / inclinationCoefficient2) + x) && (col <= ((y - row) / inclinationCoefficient1) + x)) {
                                     pixelWriter.setColor(col, row, color);
-                                } else if((angle2<=angle1)&&((col <= ((y - row) / inclinationСoefficient1) + x) || (col >= ((y - row) / inclinationСoefficient2) + x))) {
-                                pixelWriter.setColor(col, row, color);
-                            }
+                                } else if ((angle2 <= angle1) && ((col <= ((y - row) / inclinationCoefficient1) + x) || (col >= ((y - row) / inclinationCoefficient2) + x))) {
+                                    pixelWriter.setColor(col, row, color);
+                                }
                                 break;
                             case 3:
                             case 4:
-                                if ((row <= y) && (col <= ((y - row) / inclinationСoefficient1) + x)) {
+                                if ((row <= y) && (col <= ((y - row) / inclinationCoefficient1) + x)) {
                                     pixelWriter.setColor(col, row, color);
-                                } else if ((row >= y) && (col <= ((y - row) / inclinationСoefficient2) + x)) {
+                                } else if ((row >= y) && (col <= ((y - row) / inclinationCoefficient2) + x)) {
                                     pixelWriter.setColor(col, row, color);
                                 }
                                 break;
@@ -45,26 +45,26 @@ public class Rasterization {
                         break;
 
                     case 2:
-                        switch (quarterangle2) {
+                        switch (quarterAngle2) {
                             case 1:
-                                if ((col <= x) && (col <= ((y - row) / inclinationСoefficient1) + x)) {
+                                if ((col <= x) && (col <= ((y - row) / inclinationCoefficient1) + x)) {
                                     pixelWriter.setColor(col, row, color);
-                                } else if ((col >= x) && (col >= ((y - row) / inclinationСoefficient2) + x)) {
+                                } else if ((col >= x) && (col >= ((y - row) / inclinationCoefficient2) + x)) {
                                     pixelWriter.setColor(col, row, color);
                                 }
                                 break;
                             case 2:
-                                if ((angle2>=angle1)&&(col >= ((y - row) / inclinationСoefficient2) + x) && (col <= ((y - row) / inclinationСoefficient1) + x)) {
+                                if ((angle2 >= angle1) && (col >= ((y - row) / inclinationCoefficient2) + x) && (col <= ((y - row) / inclinationCoefficient1) + x)) {
                                     pixelWriter.setColor(col, row, color);
-                                }else if ((angle2<=angle1)&&((col <= ((y - row) / inclinationСoefficient1) + x) || (col >= ((y - row) / inclinationСoefficient2) + x))) {
+                                } else if ((angle2 <= angle1) && ((col <= ((y - row) / inclinationCoefficient1) + x) || (col >= ((y - row) / inclinationCoefficient2) + x))) {
                                     pixelWriter.setColor(col, row, color);
                                 }
                                 break;
                             case 3:
                             case 4:
-                                if ((row <= y) && (col <= ((y - row) / inclinationСoefficient1) + x)) {
+                                if ((row <= y) && (col <= ((y - row) / inclinationCoefficient1) + x)) {
                                     pixelWriter.setColor(col, row, color);
-                                } else if ((row >= y) && (col <= ((y - row) / inclinationСoefficient2) + x)) {
+                                } else if ((row >= y) && (col <= ((y - row) / inclinationCoefficient2) + x)) {
                                     pixelWriter.setColor(col, row, color);
                                 }
                                 break;
@@ -72,20 +72,20 @@ public class Rasterization {
                         break;
 
                     case 3:
-                        switch (quarterangle2) {
+                        switch (quarterAngle2) {
                             case 1:
                             case 2:
-                                if ((row <= y) && (col >= ((y - row) / inclinationСoefficient2) + x)) {
+                                if ((row <= y) && (col >= ((y - row) / inclinationCoefficient2) + x)) {
                                     pixelWriter.setColor(col, row, color);
-                                } else if ((row >= y) && (col >= ((y - row) / inclinationСoefficient1) + x)) {
+                                } else if ((row >= y) && (col >= ((y - row) / inclinationCoefficient1) + x)) {
                                     pixelWriter.setColor(col, row, color);
                                 }
                                 break;
                             case 3:
                             case 4:
-                                if ((angle2>=angle1)&&(col >= ((y - row) / inclinationСoefficient1) + x) && (col <= ((y - row) / inclinationСoefficient2) + x)) {
+                                if ((angle2 >= angle1) && (col >= ((y - row) / inclinationCoefficient1) + x) && (col <= ((y - row) / inclinationCoefficient2) + x)) {
                                     pixelWriter.setColor(col, row, color);
-                                }else if ((angle2<=angle1)&&((col >= ((y - row) / inclinationСoefficient1) + x) || (col <= ((y - row) / inclinationСoefficient2) + x))) {
+                                } else if ((angle2 <= angle1) && ((col >= ((y - row) / inclinationCoefficient1) + x) || (col <= ((y - row) / inclinationCoefficient2) + x))) {
                                     pixelWriter.setColor(col, row, color);
                                 }
                                 break;
@@ -93,26 +93,26 @@ public class Rasterization {
                         break;
 
                     case 4:
-                        switch (quarterangle2){
+                        switch (quarterAngle2) {
                             case 1:
                             case 2:
-                                if ((row <= y) && (col >= ((y - row) / inclinationСoefficient2) + x)) {
+                                if ((row <= y) && (col >= ((y - row) / inclinationCoefficient2) + x)) {
                                     pixelWriter.setColor(col, row, color);
-                                } else if ((row >= y) && (col >= ((y - row) / inclinationСoefficient1) + x)) {
+                                } else if ((row >= y) && (col >= ((y - row) / inclinationCoefficient1) + x)) {
                                     pixelWriter.setColor(col, row, color);
                                 }
                                 break;
                             case 3:
-                                if ((col <= x) && (col <= ((y - row) / inclinationСoefficient2) + x)) {
+                                if ((col <= x) && (col <= ((y - row) / inclinationCoefficient2) + x)) {
                                     pixelWriter.setColor(col, row, color);
-                                } else if ((col >= x) && (col >= ((y - row) / inclinationСoefficient1) + x)) {
+                                } else if ((col >= x) && (col >= ((y - row) / inclinationCoefficient1) + x)) {
                                     pixelWriter.setColor(col, row, color);
                                 }
                                 break;
                             case 4:
-                                if ((angle2>=angle1)&&(col >= ((y - row) / inclinationСoefficient1) + x) && (col <= ((y - row) / inclinationСoefficient2) + x)) {
+                                if ((angle2 >= angle1) && (col >= ((y - row) / inclinationCoefficient1) + x) && (col <= ((y - row) / inclinationCoefficient2) + x)) {
                                     pixelWriter.setColor(col, row, color);
-                                }else if ((angle1>=angle1)&&((col >= ((y - row) / inclinationСoefficient1) + x) || (col <= ((y - row) / inclinationСoefficient2) + x))) {
+                                } else if ((angle2 <= angle1) && ((col >= ((y - row) / inclinationCoefficient1) + x) || (col <= ((y - row) / inclinationCoefficient2) + x))) {
                                     pixelWriter.setColor(col, row, color);
                                 }
                                 break;
