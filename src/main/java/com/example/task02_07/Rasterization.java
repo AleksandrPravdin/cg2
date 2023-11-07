2ab7d0a00df419811cd21bbf43a1e81b3a1caeb9
Alexander Gubanov <sanygubanov4@mail.ru>
2023-11-07 18:28:46 +0300

    add interpolation color

--- src/main/java/com/example/task02_07/Rasterization.java
+++ src/main/java/com/example/task02_07/Rasterization.java
@@ -9,18 +9,21 @@ public class Rasterization {
     public static void drawSector(
             final GraphicsContext graphicsContext,
             final int x, final int y,
-            final int radius,
-            final double angle1, final double angle2,
-            final Color color) {
+            final double radius,
+            final double angle1, final double angle2) {
         final PixelWriter pixelWriter = graphicsContext.getPixelWriter();
+
         final double inclinationCoefficient1 = Math.tan(Math.toRadians(angle1));
         final double inclinationCoefficient2 = Math.tan(Math.toRadians(angle2));
         final int quarterAngle1 = defineQuarterCircle(angle1);
         final int quarterAngle2 = defineQuarterCircle(angle2);
+        final int interpolationStep = (int) Math.ceil(255 / radius);
 
-        for (int row = y - radius; row < y + radius; row++) {
+        for (int row = (int) (y - radius); row < y + radius; row++) {
+
             int xLeft = (int) (x - Math.round(Math.sqrt(radius * radius - (row - y) * (row - y))));
             int xRight = (int) (x + Math.round(Math.sqrt(radius * radius - (row - y) * (row - y))));
+
             for (int col = xLeft; col < xRight; col++) {
                 switch (quarterAngle1) {
                     case 1:
@@ -28,17 +31,17 @@ public class Rasterization {
                             case 1:
                             case 2:
                                 if ((angle2 >= angle1) && (col >= ((y - row) / inclinationCoefficient2) + x) && (col <= ((y - row) / inclinationCoefficient1) + x)) {
-                                    pixelWriter.setColor(col, row, color);
+                                    pixelWriter.setColor(col, row, calculateColor(interpolationStep, x, y, col, row));
                                 } else if ((angle2 <= angle1) && ((col <= ((y - row) / inclinationCoefficient1) + x) || (col >= ((y - row) / inclinationCoefficient2) + x))) {
-                                    pixelWriter.setColor(col, row, color);
+                                    pixelWriter.setColor(col, row, calculateColor(interpolationStep, x, y, col, row));
                                 }
                                 break;
                             case 3:
                             case 4:
                                 if ((row <= y) && (col <= ((y - row) / inclinationCoefficient1) + x)) {
-                                    pixelWriter.setColor(col, row, color);
+                                    pixelWriter.setColor(col, row, calculateColor(interpolationStep, x, y, col, row));
                                 } else if ((row >= y) && (col <= ((y - row) / inclinationCoefficient2) + x)) {
-                                    pixelWriter.setColor(col, row, color);
+                                    pixelWriter.setColor(col, row, calculateColor(interpolationStep, x, y, col, row));
                                 }
                                 break;
                         }
@@ -48,24 +51,24 @@ public class Rasterization {
                         switch (quarterAngle2) {
                             case 1:
                                 if ((col <= x) && (col <= ((y - row) / inclinationCoefficient1) + x)) {
-                                    pixelWriter.setColor(col, row, color);
+                                    pixelWriter.setColor(col, row, calculateColor(interpolationStep, x, y, col, row));
                                 } else if ((col >= x) && (col >= ((y - row) / inclinationCoefficient2) + x)) {
-                                    pixelWriter.setColor(col, row, color);
+                                    pixelWriter.setColor(col, row, calculateColor(interpolationStep, x, y, col, row));
                                 }
                                 break;
                             case 2:
                                 if ((angle2 >= angle1) && (col >= ((y - row) / inclinationCoefficient2) + x) && (col <= ((y - row) / inclinationCoefficient1) + x)) {
-                                    pixelWriter.setColor(col, row, color);
+                                    pixelWriter.setColor(col, row, calculateColor(interpolationStep, x, y, col, row));
                                 } else if ((angle2 <= angle1) && ((col <= ((y - row) / inclinationCoefficient1) + x) || (col >= ((y - row) / inclinationCoefficient2) + x))) {
-                                    pixelWriter.setColor(col, row, color);
+                                    pixelWriter.setColor(col, row, calculateColor(interpolationStep, x, y, col, row));
                                 }
                                 break;
                             case 3:
                             case 4:
                                 if ((row <= y) && (col <= ((y - row) / inclinationCoefficient1) + x)) {
-                                    pixelWriter.setColor(col, row, color);
+                                    pixelWriter.setColor(col, row, calculateColor(interpolationStep, x, y, col, row));
                                 } else if ((row >= y) && (col <= ((y - row) / inclinationCoefficient2) + x)) {
-                                    pixelWriter.setColor(col, row, color);
+                                    pixelWriter.setColor(col, row, calculateColor(interpolationStep, x, y, col, row));
                                 }
                                 break;
                         }
@@ -76,17 +79,17 @@ public class Rasterization {
                             case 1:
                             case 2:
                                 if ((row <= y) && (col >= ((y - row) / inclinationCoefficient2) + x)) {
-                                    pixelWriter.setColor(col, row, color);
+                                    pixelWriter.setColor(col, row, calculateColor(interpolationStep, x, y, col, row));
                                 } else if ((row >= y) && (col >= ((y - row) / inclinationCoefficient1) + x)) {
-                                    pixelWriter.setColor(col, row, color);
+                                    pixelWriter.setColor(col, row, calculateColor(interpolationStep, x, y, col, row));
                                 }
                                 break;
                             case 3:
                             case 4:
                                 if ((angle2 >= angle1) && (col >= ((y - row) / inclinationCoefficient1) + x) && (col <= ((y - row) / inclinationCoefficient2) + x)) {
-                                    pixelWriter.setColor(col, row, color);
+                                    pixelWriter.setColor(col, row, calculateColor(interpolationStep, x, y, col, row));
                                 } else if ((angle2 <= angle1) && ((col >= ((y - row) / inclinationCoefficient1) + x) || (col <= ((y - row) / inclinationCoefficient2) + x))) {
-                                    pixelWriter.setColor(col, row, color);
+                                    pixelWriter.setColor(col, row, calculateColor(interpolationStep, x, y, col, row));
                                 }
                                 break;
                         }
@@ -97,23 +100,23 @@ public class Rasterization {
                             case 1:
                             case 2:
                                 if ((row <= y) && (col >= ((y - row) / inclinationCoefficient2) + x)) {
-                                    pixelWriter.setColor(col, row, color);
+                                    pixelWriter.setColor(col, row, calculateColor(interpolationStep, x, y, col, row));
                                 } else if ((row >= y) && (col >= ((y - row) / inclinationCoefficient1) + x)) {
-                                    pixelWriter.setColor(col, row, color);
+                                    pixelWriter.setColor(col, row, calculateColor(interpolationStep, x, y, col, row));
                                 }
                                 break;
                             case 3:
                                 if ((col <= x) && (col <= ((y - row) / inclinationCoefficient2) + x)) {
-                                    pixelWriter.setColor(col, row, color);
+                                    pixelWriter.setColor(col, row, calculateColor(interpolationStep, x, y, col, row));
                                 } else if ((col >= x) && (col >= ((y - row) / inclinationCoefficient1) + x)) {
-                                    pixelWriter.setColor(col, row, color);
+                                    pixelWriter.setColor(col, row, calculateColor(interpolationStep, x, y, col, row));
                                 }
                                 break;
                             case 4:
                                 if ((angle2 >= angle1) && (col >= ((y - row) / inclinationCoefficient1) + x) && (col <= ((y - row) / inclinationCoefficient2) + x)) {
-                                    pixelWriter.setColor(col, row, color);
+                                    pixelWriter.setColor(col, row, calculateColor(interpolationStep, x, y, col, row));
                                 } else if ((angle2 <= angle1) && ((col >= ((y - row) / inclinationCoefficient1) + x) || (col <= ((y - row) / inclinationCoefficient2) + x))) {
-                                    pixelWriter.setColor(col, row, color);
+                                    pixelWriter.setColor(col, row, calculateColor(interpolationStep, x, y, col, row));
                                 }
                                 break;
                         }
@@ -136,5 +139,12 @@ public class Rasterization {
         }
         return 0;
     }
-}
 
+    private static Color calculateColor(int interpolationStep, int x, int y, int col, int row) {
+        int rgbOffset = (int) (interpolationStep * Math.round(Math.sqrt((x - col) * (x - col) + (y - row) * (y - row))));
+        if (rgbOffset > 255) {
+            rgbOffset = 255;
+        }
+        return Color.rgb(255 - rgbOffset, 0, rgbOffset);
+    }
+}
--- src/main/java/com/example/task02_07/RasterizationController.java
+++ src/main/java/com/example/task02_07/RasterizationController.java
@@ -18,10 +18,31 @@ public class RasterizationController {
         anchorPane.prefWidthProperty().addListener((ov, oldValue, newValue) -> canvas.setWidth(newValue.doubleValue()));
         anchorPane.prefHeightProperty().addListener((ov, oldValue, newValue) -> canvas.setHeight(newValue.doubleValue()));
 
-        Rasterization.drawSector(canvas.getGraphicsContext2D(),250,250,100,30.0,330.0,Color.YELLOW);
-        Rasterization.drawSector(canvas.getGraphicsContext2D(),290,190,15,330.0,330.0,Color.BLACK);
-        Rasterization.drawSector(canvas.getGraphicsContext2D(),450,250,65,225.0,45.0,Color.GREEN);
-        Rasterization.drawSector(canvas.getGraphicsContext2D(),450,250,55,225.0,45.0,Color.RED);
+        Rasterization.drawSector(canvas.getGraphicsContext2D(), 110, 103, 90, 20, 70);
+        Rasterization.drawSector(canvas.getGraphicsContext2D(), 310, 103, 90, 20, 160);
+        Rasterization.drawSector(canvas.getGraphicsContext2D(), 510, 103, 90, 20, 250);
+        Rasterization.drawSector(canvas.getGraphicsContext2D(), 710, 103, 90, 20, 330);
+        Rasterization.drawSector(canvas.getGraphicsContext2D(), 910, 103, 90, 70, 20);
+
+        Rasterization.drawSector(canvas.getGraphicsContext2D(), 110, 303, 90, 110, 160);
+        Rasterization.drawSector(canvas.getGraphicsContext2D(), 310, 303, 90, 110, 250);
+        Rasterization.drawSector(canvas.getGraphicsContext2D(), 510, 303, 90, 110, 330);
+        Rasterization.drawSector(canvas.getGraphicsContext2D(), 710, 303, 90, 110, 20);
+        Rasterization.drawSector(canvas.getGraphicsContext2D(), 910, 303, 90, 160, 110);
+
+        Rasterization.drawSector(canvas.getGraphicsContext2D(), 110, 503, 90, 200, 250);
+        Rasterization.drawSector(canvas.getGraphicsContext2D(), 310, 503, 90, 200, 330);
+        Rasterization.drawSector(canvas.getGraphicsContext2D(), 510, 503, 90, 200, 70);
+        Rasterization.drawSector(canvas.getGraphicsContext2D(), 710, 503, 90, 200, 160);
+        Rasterization.drawSector(canvas.getGraphicsContext2D(), 910, 503, 90, 250, 200);
+
+        Rasterization.drawSector(canvas.getGraphicsContext2D(), 110, 703, 90, 290, 340);
+        Rasterization.drawSector(canvas.getGraphicsContext2D(), 310, 703, 90, 290, 20);
+        Rasterization.drawSector(canvas.getGraphicsContext2D(), 510, 703, 90, 290, 160);
+        Rasterization.drawSector(canvas.getGraphicsContext2D(), 710, 703, 90, 290, 250);
+        Rasterization.drawSector(canvas.getGraphicsContext2D(), 910, 703, 90, 340, 290);
+
+        Rasterization.drawSector(canvas.getGraphicsContext2D(), 1240, 400, 150, 340, 340);
     }
 
 }
